3cd6594b55ed429ae57b21192e50c5108344227c
unknown <tirusatrapu36@gmail.com>
2025-09-25 10:34:46 +0530

    expense-project

--- EC2-instance.sh
+++ EC2-instance.sh
@@ -3,8 +3,13 @@
 AMI_ID=ami-09c813fb71547fc4f
 SG_ID=sg-0f3a1afbf0bbc7f0e
 
-aws ec2 run-instances \
-    --image-id "$AMI_ID" \
-    --instance-type "t3.micro" \
-    --security-group-ids "$SG_ID" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=text}]"
+for instance in $@
+do
+	aws ec2 run-instances \
+		--image-id "$AMI_ID" \
+		--instance-type "t3.micro" \
+		--security-group-ids "$SG_ID" \
+		--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"
+		--query 'Instances[0].InstanceId' \
+		--output text
+done
