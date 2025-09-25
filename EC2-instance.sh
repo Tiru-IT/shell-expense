4072be5472478e0a4e7ebab274dec58e656ceb0c
unknown <tirusatrapu36@gmail.com>
2025-09-25 10:44:58 +0530

    expense-project

--- EC2-instance.sh
+++ EC2-instance.sh
@@ -5,11 +5,23 @@ SG_ID=sg-0f3a1afbf0bbc7f0e
 
 for instance in $@
 do
-	aws ec2 run-instances \
+	INSTANCE_ID=(aws ec2 run-instances \
 		--image-id "$AMI_ID" \
 		--instance-type "t3.micro" \
 		--security-group-ids "$SG_ID" \
 		--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-	    --query 'Instances[0].InstanceId' \
-		--output text
+		--query 'Instances[0].InstanceId' \
+		--output text)
+	if [ $instance != frontend ]; then
+		IP=$(aws ec2 describe-instances \
+			  --instance-ids "$INSTANCE_ID" \
+			  --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+			  --output text)
+	else
+		IP=$(aws ec2 describe-instances \
+			  --instance-ids "$INSTANCE_ID" \
+			  --query 'Reservations[0].Instances[0].PublicIpAddress' \
+			  --output text)
+	fi		
+		
 done
