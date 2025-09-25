1c1ff5b0dfd9500a536d5a7f9591982bc3e849fd
unknown <tirusatrapu36@gmail.com>
2025-09-25 10:24:29 +0530

    expense-project

---
+++ EC2-instance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+AMI_ID=ami-09c813fb71547fc4f
+SG_ID=sg-0f3a1afbf0bbc7f0e
+
+aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --instance-type "tmicro" \
+    --security-group-ids "$SG_ID" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=text}]"
